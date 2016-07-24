1bd286a3e68c6f96415acea8c262f3812b307e3e
Julian Kahnert <mail@juliankahnert.de>
2016-07-24 12:14:58 +0200

    FreeBSD init added

---
+++ BSD/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# get ports the first time
+su
+portsnap fetch
+portsnap extract
+
+# install portmaster
+cd /usr/ports/ports-mgmt/portmaster/ && make install
+
+# install ports
+portmaster /usr/ports/shells/zsh
+portmaster /usr/ports/sysutils/ezjail
+
+# install some packages
+pkg install security/sudo devel/git editors/vim
+pkg install zfstools

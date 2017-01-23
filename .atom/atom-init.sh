d94dd93dfa9f7956ee0d8f5b42eabdaad2d258de
Julian Kahnert <mail@juliankahnert.de>
2017-01-23 10:43:54 +0100

    more packages added

--- .atom/atom-init.sh
+++ .atom/atom-init.sh
@@ -7,6 +7,7 @@
 brew cask install atom
 
 # install packages
+apm install auto-detect-indentation
 apm install autocomplete-python
 apm install fold-functions
 apm install git-plus
@@ -14,12 +15,14 @@ apm install highlight-selected
 apm install language-ini
 apm install language-latex
 apm install latex
+apm install latexer
 apm install linter
 apm install linter-pep8
 apm install markdown-scroll-sync
 apm install multi-cursor
 apm install package-sync
 apm install python-indent
+apm install ssh-config
 apm install synced-sidebar
 apm install todo-show
 apm install tomorrow-night-eighties-syntax

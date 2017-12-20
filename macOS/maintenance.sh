2b4e96cf983db96086c5da53139b4d4e7484f35c
Julian Kahnert <kahnert@otaris.de>
2017-12-20 08:58:44 +0100

    update maintenance scripts

--- macOS/maintenance.sh
+++ macOS/maintenance.sh
@@ -15,10 +15,6 @@ echo "---------------------------------------------------------------"
 # keep MacAppStore Apps up-to-date
 mas upgrade
 
-echo "#   PYTHON PACKAGES   #########################################"
-# update all python3 packages
-pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install --upgrade --user > /dev/null
-
 echo "#   TIME MACHINE BACKUP   #####################################"
 # start Time Machine backup
 #tmutil startbackup

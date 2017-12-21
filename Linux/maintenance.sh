ff059b2ff1cb7c70ca52ce9d26adf4f512180e1b
Julian Kahnert <mail@juliankahnert.de>
2017-12-21 11:23:01 +0100

    Linux: some minor fixes

--- Linux/maintenance.sh
+++ Linux/maintenance.sh
@@ -5,14 +5,13 @@ sudo -v
 echo "#   SYSTEM   ##################################################"
 # Security updates only
 sudo apt update
-sudo apt -yt $(lsb_release -cs)-security dist-upgrade
 sudo apt --trivial-only dist-upgrade
 
 echo "#   UPDATE   ##################################################"
 sudo apt --yes upgrade
 
 echo "#   CLEANUP   #################################################"
-sudo apt autoremove
-sudo apt autoclean
+sudo apt-get autoremove
+sudo apt-get autoclean
 
 echo "###############################################################"

d4777327e26eeeb399baaa90a3b6a66eecd625c9
Julian Kahnert <mail@juliankahnert.de>
2026-02-07 23:01:53 +0100

    Enhance cursor visibility in Neovim with guicursor configuration

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -8,3 +8,12 @@ vim.opt.expandtab = true
 
 -- Cursor visibility
 vim.opt.cursorline = true -- Highlight the current line
+
+-- Make cursor more visible with thicker block cursor
+vim.opt.guicursor = {
+  "n-v-c:block-Cursor/lCursor", -- Normal, visual, command: block cursor
+  "i-ci-ve:ver25-Cursor/lCursor", -- Insert: vertical bar at 25% width
+  "r-cr:hor20-Cursor/lCursor", -- Replace: horizontal bar at 20% height
+  "o:hor50-Cursor/lCursor", -- Operator-pending: horizontal bar at 50%
+  "a:blinkwait700-blinkoff400-blinkon250", -- All modes: cursor blink settings
+}

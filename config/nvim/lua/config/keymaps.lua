352b9330647e4e444b9ca8f53310083bea5b18a6
Kahnert, Julian <Julian.Kahnert@ewe.de>
2026-02-19 08:56:03 +0100

    disable macro recording in LazyVim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Disable macro recording (q is never used intentionally)
+vim.keymap.set("n", "q", "<nop>", { desc = "Disable macro recording" })
